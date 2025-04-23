1b3614105075d785ef8d2e94e20d5967402c8b86
jari.nxt <jari_nxt@hotmail.com>
2025-04-23 10:30:29 -0700

    Fixed Shell Script to compile all current libraries

--- build_libs.sh
+++ build_libs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Compile the Pascal source file
-fpc ./src/beatrice_v.front.lib.pas
-fpc ./src/beatrice_v.engine.lib.pas
 fpc ./src/beatrice_v.net.lib.pas
+fpc ./src/beatrice_v.back.lib.pas
+fpc ./src/beatrice_v.mgmt.lib.pas
+fpc ./src/beatrice_v.math.lib.pas
+fpc ./src/beatrice_v.front.lib.pas
 
 # Remove object files and compiled unit files
 rm ./src/*.o
